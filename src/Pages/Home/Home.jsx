d394b43ba0422ac7a59d7865fe37348478d5d732
Henrique Rinaldi <henriquergr@hotmail.com>
2021-10-11 15:50:47 -0300

    feat: implemented first section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,34 @@
+import React from 'react';
+
+import Workers from './../../Assets/workers.svg';
+import Logo from './../../Assets/logo.png';
+import Header from './../../Assets/header.svg';
+
+import './Home.css';
+
 const Home = () => {
+
     return (
-        <h1>Hello World</h1>
+        <React.Fragment>                
+            <div className='container bgHeader pt-2 pb-3'>
+                <div className="container">
+                    <img className='w-25' src={Logo} alt='Logo' />
+                    <p className='title fw-bold fs-1 mb-0 text-center'>ATIVIDADES PRESENCIAIS</p>
+                </div>
+            </div>
+            <div className='container bgLanding text-center'>
+                <div className='container text-content'>
+                    <img className='img-fluid' src={Workers} alt='Funcionários se cumprimentando' />
+                    <p>
+                        agora você <span className='orange'>#sanguelaranja</span> tem a
+                        <span className='orange'> opção</span> de trabalhar em nossos
+                        escritórios!
+                    </p>
+                    <input className='bgOrange w-100 rounded-2 p-2' type='button' value='Realizar Agendamento' />
+                    <span className='label'>todos os protocolos serão respeitados.</span>
+                </div>
+            </div>
+        </React.Fragment>
     )
 }
 
